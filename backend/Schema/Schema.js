f3ab72e019c116084e775194524a21f9c6b5d89d
khushveer <khushveermalviya26@gmail.com>
2024-11-25 22:13:35 +0530

    second

--- backend/Schema/Schema.js
+++ backend/Schema/Schema.js
@@ -1,31 +1,42 @@
 import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLInt } from 'graphql';
 import Axios from 'axios';
 
-
+// Define SchoolType (for Company data)
 const SchoolType = new GraphQLObjectType({
   name: "School",
-  fields:{
-    id:{ type: GraphQLString},
-    name :{type: GraphQLString}
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    user:{
+      type : new GraphQLList (UserType),
+      resolve(parentValue,args){
+        return Axios.get(`http://localhost:3000/companies/${parentValue.id}/User`)
+      }
+    }
   }
-})
+});
 
+// Define UserType
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstname: { type: GraphQLString },
     age: { type: GraphQLInt },
-    company : {
-      type : CompanyType,
-      resolve(parentValue,args){
-        console.log(parentValue,args);
-        
+    company: {
+      type: SchoolType,
+      resolve(parentValue, args) {
+        return Axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data)
+          .catch(error => {
+            throw new Error('An error occurred while fetching the company data');
+          });
       }
     }
   }
 });
 
+// Define RootQuery
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -39,10 +50,22 @@ const RootQuery = new GraphQLObjectType({
             throw new Error('An error occurred while fetching the user data');
           });
       }
+    },
+    company :{
+      type: SchoolType,
+      args:{id:{type: GraphQLString}},
+      resolve(parentValue,args){
+        return Axios.get(`http://localhost:3000/companies/${args.id}`)
+        .then(res=>res.data)
+        .catch(error=>{
+          throw new error("a problem a hey bhau")
+        })
+      }
     }
   }
 });
 
+// Define Schema
 const schema = new GraphQLSchema({
   query: RootQuery
 });
