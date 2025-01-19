ae404eb3a486bd2347254a9dbabcb6aa9cc50245
khushveer <khushveermalviya26@gmail.com>
2025-01-19 09:36:54 +0530

    ad

--- frontend/Apolloclient.jsx
+++ frontend/Apolloclient.jsx
@@ -2,12 +2,12 @@ import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/c
 const cnt = new ApolloClient({
     link: new HttpLink({
       // uri: 'https://backend-kz3r.onrender.com/graphql',
-      uri:"http://localhost:5000/graphql",
-      // uri:'https://center-gefucegncpf7akcc.centralindia-01.azurewebsites.net/graphql',
+      // uri:"http://localhost:5000/graphql",
+      uri:'https://center-gefucegncpf7akcc.centralindia-01.azurewebsites.net/graphql',
       headers: {
         Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
     }),
     cache: new InMemoryCache(),
   });
-  export default cnt;
+  export default cnt; 
