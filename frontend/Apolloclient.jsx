4af44e866a765a536a5c0e1ab7a606d334421494
khushveer <khushveermalviya26@gmail.com>
2025-03-05 23:30:02 +0530

    adsfa

--- frontend/Apolloclient.jsx
+++ frontend/Apolloclient.jsx
@@ -9,8 +9,8 @@
     import { getMainDefinition } from '@apollo/client/utilities';
 
     // Your Azure App Service endpoint
-    const serverEndpoint = 'http://localhost:5000';
-    // 
+    const serverEndpoint = 'https://center-gefucegncpf7akcc.centralindia-01.azurewebsites.net/graphql';
+    // https://center-gefucegncpf7akcc.centralindia-01.azurewebsites.net/graphql
     //http://localhost:5000
     // HTTP Link
     const httpLink = new HttpLink({
